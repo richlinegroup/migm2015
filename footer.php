08f5555fdc90063f50b002da9629733202a287c9
JonK_RLG <Jonathan.Kelly@richlinegroup.com>
2015-03-02 16:36:54 -0500

    updated the footer with the logos

--- footer.php
+++ footer.php
@@ -1,8 +1,27 @@
 <footer>
-	<div class="container-fluid">
+	<div class="container-fluid yellowBackground">
 		<div class="row">
-			<div class="col-sm-12">
-				<!-- ** Retailer logos go here ** -->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">						
+						<ul class="list-inline text-center">
+							<li><img src="img/logos/amazonLogo.png"></li>
+							<li><img src="img/logos/bjLogo.png"></li>
+							<li><img src="img/logos/fredMeyerLogo.png"></li>
+							<li><img src="img/logos/helzbergLogo.png"></li>
+							<li><img src="img/logos/hsnLogo.png"></li>
+							<li><img src="img/logos/jcpLogo.png"></li>
+							<li><img src="img/logos/kayJewelerLogo.png"></li>
+							<li><img src="img/logos/kmartLogo.png"></li>
+							<li><img src="img/logos/kohlsLogo.png"></li>
+							<li><img src="img/logos/macysLogo.png"></li>
+							<li><img src="img/logos/qvcLogo.png"></li>
+							<li><img src="img/logos/samsLogo.png"></li>
+							<li><img src="img/logos/searsLogo.png"></li>
+							<li><img src="img/logos/walmartLogo.png"></li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -11,13 +30,13 @@
 			<div class="col-xs-12 col-sm-6 col-md-6 col-sm-offset-3 col-md-offset-3 text-center">
 				<small>Copyright &copy;2015 Richline Group. All rights reserved. This website, and the images, design, logos, trademarks
 					and trade names contained herein, other then those of our participating retailers, arethe exclusive property of Richline Group, Inc. 
-					Any unauthorized use will nr prosecuted to the fullest extent of the law.</small>
+					Any unauthorized use will nr prosecuted to the fullest extent of the law.</small> <br><br>
 			</div>
 		</div>
 	</div>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-xs-12 yellowBG"><br></div>	
+			<div class="col-xs-12 yellowBackground"><br></div>	
 		</div>
 	</div>
 </footer>
