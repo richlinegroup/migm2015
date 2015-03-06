e5234efe845be4101bfadf2f79952549bd109d02
irinamanning <irina.manning@ny-10352.richlinegroup.local>
2015-03-06 14:42:30 -0500

    fixed slides (made them 100% screen width)

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
   require_once('header.php');
 ?>
   <div class="main">
-    <div id="mainCarousel" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
+    <div id="mainCarousel" class="carousel slide" data-ride="carousel" >
+      <ol class="carousel-indicators" >
         <li data-target="mainCarousel" data-slide-to="0" class="active"></li>
         <li data-target="mainCarousel" data-slide-to="1"></li>
         <li data-target="mainCarousel" data-slide-to="2"></li>
       </ol>
 
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="img/main-banner-1.png" alt="" />
+      <div class="carousel-inner" role="listbox" >
+        <div class="item active" >
+          <img src="img/main-banner-1.png" alt="" style="width: 100%"/>
         </div>
         <div class="item">
           <img src="img/main-banner-1.png" alt="" />
