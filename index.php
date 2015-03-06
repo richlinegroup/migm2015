c6206eab0019f8cdb4b4b3cdd34e844b7c64768c
irinamanning <irina.manning@ny-10352.richlinegroup.local>
2015-03-06 16:38:31 -0500

    edited the layout

--- index.php
+++ index.php
@@ -48,11 +48,11 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-6 col-md-6">
-          <!-- ** PIN TO WIN ** -->
+          <img src="img/pinterest-banner.png" />
         </div>
 
         <div class="col-sm-6 col-md-6">
-          <!-- ** PIC TO WIN ** -->
+          <img src="img/instagram-banner.png" />
         </div>
       </div>
     </div>
@@ -63,27 +63,77 @@
       </div>
     </div>
 
-    <div class="container">
+    <div class="container-fluid"> 
+      <div class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="col-sm-4">
+          <img src="img/new-expert.png" class="img-responsive"/>
+        </div>
+        <div class="col-sm-7">
+        </div>
+      </div>
 
+      <div class="row">
+        <div class="col-sm-1">
+        </div>
+        <div class="col-sm-2">
+          <img src="img/expert-1.png" class="img-responsive"/>
+          <img src="img/expert-2.png" class="img-responsive"/>
+        </div>
+        <div class="col-sm-9">
+        </div>
+      </div>
     </div>
 
     <div class="row">
       <div class="col-sm-12 text-center">
         <h2 class="heading brownFont yellowBackground">GOLD EDUCATION</h2>
+        <img src="img/education-banner.png" class="img-responsive" id="education-image"/>
       </div>
     </div>
 
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <h2 class="heading brownFont yellowBackground">TREND OF THE WEEK</h2>
+    </div>
+  </div>
+
+  <div class="container-fluid"> 
     <div class="row">
       <div class="col-sm-12 text-center">
-        <h2 class="heading brownFont yellowBackground">TREND OF THE WEEK</h2>
+        <img src="img/trend-banner.png" class="img-responsive" id="trends-image"/>
       </div>
     </div>
+  </div> 
 
     <div class="row">
       <div class="col-sm-12 text-center">
         <h2 class="heading brownFont yellowBackground">ABOUT</h2>
       </div>
     </div>
+
+    <div class="container-fluid"> 
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <p>Celebrate Your Love For Gold!<br>
+
+             May is time for Mother's Day, graduations, bridal showers, weddings and anniversaries. It also marks the beginning of our 11th annual May is Gold Month event! This year's event offers more ways than ever before to show your love of Gold:<br>
+
+             - Win $1000 Gold Shopping Spree from one of participating retailers  Learn More<br>
+             - Check out the latest in gold trends and jewelry fashion<br>
+             - Educate yourself about gold by getting expert insight<br> 
+             - Share your gold style on Instagram and get a chance to win a 14k Gold Bracelet!  Learn More<br>
+             - Discover new gold looks on Pinterest and get a chance to win 14k Gold Earrings!  Learn More<br>
+             - Get an email update with the most current and interesting from May Is Gold Month <br>  
+             
+             Become an Insider<br>
+             Receive a weekly update with the latest from May is Gold Month.<br>
+          </p>
+        </div>
+      </div>
+    </div>
+
 
     <?php include('footer.php'); ?>
   </div>
