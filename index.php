0300840f4a18ba486dd8be11fe41189c88017625
irinamanning <irina.manning@ny-10352.richlinegroup.local>
2015-03-09 15:00:19 -0400

    changes to trends and about sections

--- index.php
+++ index.php
@@ -1,140 +1,146 @@
 <?php
   require_once('header.php');
 ?>
-  <div class="main">
-    <div id="mainCarousel" class="carousel slide" data-ride="carousel" >
-      <ol class="carousel-indicators" >
-        <li data-target="mainCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="mainCarousel" data-slide-to="1"></li>
-        <li data-target="mainCarousel" data-slide-to="2"></li>
-      </ol>
+  <div style="width: 100%" class="background">
+    <div class="main dropShadowMain">
+      <div id="mainCarousel" class="carousel slide" data-ride="carousel" >
+        <ol class="carousel-indicators" >
+          <li data-target="mainCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="mainCarousel" data-slide-to="1"></li>
+          <li data-target="mainCarousel" data-slide-to="2"></li>
+        </ol>
 
-      <div class="carousel-inner" role="listbox" >
-        <div class="item active" >
-          <img src="img/main-banner-1.png" alt="" class="slide"/>
-        </div>
-        <div class="item">
-          <img src="img/main-banner-1.png" alt="" class="slide"/>
-        </div>
-        <div class="item">
-          <img src="img/main-banner-1.png" alt="" class="slide"/>
-        </div>
-      </div>
-    </div>
-
-
-   
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <h2 class="heading brownFont yellowBackground">PARTICIPATING RETAILERS</h2>
-      </div>
-    </div>
-
-
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-            <!-- *** Retailer logos go here *** -->
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <h2 class="heading brownFont yellowBackground">SOCIAL MEDIA SWEEPSTAKES</h2>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-6 col-md-6">
-          <img src="img/pinterest-banner.png" />
-        </div>
-
-        <div class="col-sm-6 col-md-6">
-          <img src="img/instagram-banner.png" />
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <h2 class="heading brownFont yellowBackground">EXPERT INTERVIEWS</h2>
-      </div>
-    </div>
-
-    <div class="container-fluid"> 
-      <div class="row">
-        <div class="col-sm-1">
-        </div>
-        <div class="col-sm-4">
-          <img src="img/new-expert.png" class="img-responsive"/>
-        </div>
-        <div class="col-sm-7">
+        <div class="carousel-inner" role="listbox" >
+          <div class="item active" >
+            <img src="img/main-banner-1.png" alt="" class="slide"/>
+          </div>
+          <div class="item">
+            <img src="img/main-banner-1.png" alt="" class="slide"/>
+          </div>
+          <div class="item">
+            <img src="img/main-banner-1.png" alt="" class="slide"/>
+          </div>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-sm-1">
-        </div>
-        <div class="col-sm-2">
-          <img src="img/expert-1.png" class="img-responsive"/>
-          <img src="img/expert-2.png" class="img-responsive"/>
-        </div>
-        <div class="col-sm-9">
-        </div>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <h2 class="heading brownFont yellowBackground">GOLD EDUCATION</h2>
-        <img src="img/education-banner.png" class="img-responsive" id="education-image"/>
-      </div>
-    </div>
-
-  <div class="row">
-    <div class="col-sm-12 text-center">
-      <h2 class="heading brownFont yellowBackground">TREND OF THE WEEK</h2>
-    </div>
-  </div>
-
-  <div class="container-fluid"> 
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <img src="img/trend-banner.png" class="img-responsive" id="trends-image"/>
-      </div>
-    </div>
-  </div> 
-
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <h2 class="heading brownFont yellowBackground">ABOUT</h2>
-      </div>
-    </div>
-
-    <div class="container-fluid"> 
+     
       <div class="row">
         <div class="col-sm-12 text-center">
-          <p>Celebrate Your Love For Gold!<br>
+          <h2 class="heading brownFont yellowBackground">PARTICIPATING RETAILERS</h2>
+        </div>
+      </div>
 
-             May is time for Mother's Day, graduations, bridal showers, weddings and anniversaries. It also marks the beginning of our 11th annual May is Gold Month event! This year's event offers more ways than ever before to show your love of Gold:<br>
 
-             - Win $1000 Gold Shopping Spree from one of participating retailers  Learn More<br>
-             - Check out the latest in gold trends and jewelry fashion<br>
-             - Educate yourself about gold by getting expert insight<br> 
-             - Share your gold style on Instagram and get a chance to win a 14k Gold Bracelet!  Learn More<br>
-             - Discover new gold looks on Pinterest and get a chance to win 14k Gold Earrings!  Learn More<br>
-             - Get an email update with the most current and interesting from May Is Gold Month <br>  
-             
-             Become an Insider<br>
-             Receive a weekly update with the latest from May is Gold Month.<br>
-          </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+              <!-- *** Retailer logos go here *** -->
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h2 class="heading brownFont yellowBackground">SOCIAL MEDIA SWEEPSTAKES</h2>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-md-6">
+            <img src="img/pinterest-banner.png" />
+          </div>
+
+          <div class="col-sm-6 col-md-6">
+            <img src="img/instagram-banner.png" />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h2 class="heading brownFont yellowBackground">EXPERT INTERVIEWS</h2>
+        </div>
+      </div>
+
+      <div class="container-fluid"> 
+        <div class="row">
+          <div class="col-sm-1">
+          </div>
+          <div class="col-sm-4">
+            <img src="img/new-expert.png" class="img-responsive"/>
+          </div>
+          <div class="col-sm-7">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-1">
+          </div>
+          <div class="col-sm-2">
+            <img src="img/expert-1.png" class="img-responsive"/>
+            <img src="img/expert-2.png" class="img-responsive"/>
+          </div>
+          <div class="col-sm-9">
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h2 class="heading brownFont yellowBackground">GOLD EDUCATION</h2>
+          <img src="img/education-banner.png" class="img-responsive" id="education-image"/>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h2 class="heading brownFont yellowBackground">TREND OF THE WEEK</h2>
+          <p class="education-title"> </p>
+          <p class="education-quote"> </p>
+        </div>
+      </div>
+
+      <div class="container-fluid"> 
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <img src="img/trend-banner.png" class="img-responsive" id="trends-image"/>
+            <p class="link"><a href="#"><u>VIEW TREND</u></a></p>
+          </div>
+        </div>
+      </div> 
+
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h2 class="heading brownFont yellowBackground">ABOUT</h2>
+        </div>
+      </div>
+
+      <div class="container-fluid"> 
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <div class="about">  
+              <p class="about-subtitle yellowFont">Celebrate Your Love For Gold!</p>
+
+              <p>May is time for Mother's Day, graduations, bridal showers, weddings and anniversaries. It also marks the beginning of our 11th annual May is Gold Month event! This year's event offers more ways than ever before to show your love of Gold:</p>
+
+              <p><b>- Win $1000 Gold Shopping Spree from one of participating retailers</b> <a href="#" class="brownFont"><u>Learn More</u></a><br>
+              <b>- Check out the latest in gold trends and jewelry fashion<br>
+              - Educate yourself about gold by getting expert insight<br> 
+              - Share your gold style on Instagram and get a chance to win a 14k Gold Bracelet!</b> <a href="#" class="brownFont"><u>Learn More</u></a><br>
+              <b>- Discover new gold looks on Pinterest and get a chance to win 14k Gold Earrings!</b> <a href="#" class="brownFont"><u>Learn More</u></a><br>
+              <b>- Get an email update with the most current and interesting from May Is Gold Month</b></p> 
+                 
+              <p class="about-subtitle yellowFont">Become an Insider</p>
+              <p>Receive a weekly update with the latest from May is Gold Month.</p>
+              </p>
+            </div>  
+          </div>
         </div>
       </div>
     </div>
-
-
+  </div>
     <?php include('footer.php'); ?>
   </div>
 </body>
