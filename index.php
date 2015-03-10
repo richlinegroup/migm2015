88f6ff96b3ad9b82eb07aeb5823ce3f530c2ee6c
irinamanning <irina.manning@ny-10352.richlinegroup.local>
2015-03-10 11:33:02 -0400

    Finished editing the look of the about section

--- index.php
+++ index.php
@@ -139,6 +139,17 @@
                  
               <p class="about-subtitle yellowFont">Become an Insider</p>
               <p>Receive a weekly update with the latest from May is Gold Month.</p>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="input-group input-group-lg text-center" style="width: 500px; margin: auto" >
+                    <input type="text" class="form-control email-field-footer" placeholder="Enter Email Address" style="height: 60px; border: 1px solid #684f40; border-radius: 0px; font-size: 16px; margin-top: 15px;">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default sign-up-footer yellowFont" type="button" style="height: 60px; border: 1px solid #684f40; border-radius: 0px; font-size: 16px; margin-top: 15px; background: #684f40">SIGN UP</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <p class="brownFont note">**Insiders will automatically be entered to win a $1000 Gold Jewelry shopping spree!</p>
               </p>
             </div>  
           </div>
