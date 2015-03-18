2cfca861dd4003cebab9d5c149732044996fe8ae
JonK_RLG <Jonathan.Kelly@richlinegroup.com>
2015-03-18 10:21:43 -0400

    added a png version of the logo as well as updated the header with the png migm logo

--- header.php
+++ header.php
@@ -1,11 +1,3 @@
-<?php //require_once( 'portal/cms.php' ); ?>
-
-<!-- <cms:template title="Homepage Template">
-  <cms:editable name="blog_main_image" crop="1" type="image" />
-  <cms:editable name="blog_title" type="text" />
-  <cms:editable name="blog_excerpt" type="richtext" />
-  <cms:editable name="blog_url" crop="1" type="image" />
-</cms:template> -->
 <!DOCTYPE html>
 <!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
 <html lang="en" >
@@ -19,7 +11,7 @@
 
     <title>MAY IS GOLD MONTH</title>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 
@@ -31,42 +23,91 @@
        #mc_embed input.email { padding-top: 0.35em; padding-bottom: 0.35em; }
     </style>
 
-
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-16677706-5', 'mayisgoldmonth.com');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+    </script>
   </head>
 
   <body>
     <div class="container-fluid">
       <div class="row topMargin">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-4">
-              <img src="img/migm_logo.svg" alt="May Is Gold Month" />
-            </div>
-          
-            <div class="col-sm-5 col-sm-offset-3">
-                <ul class="list-inline">
-                  <li><i class="fa fa-facebook fa-2x yellowFont textShadow"></i></li>
-                  <li><i class="fa fa-twitter fa-2x yellowFont textShadow"></i></li>
-                  <li><i class="fa fa-instagram fa-2x yellowFont textShadow"></i></li>
-                  <li><i class="fa fa-pinterest fa-2x yellowFont textShadow"></i></li>
+        <div class="col-xs-12">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-4">
+                <img src="img/migm_logo.png" alt="May Is Gold Month" />
+              </div>
+            
+              <div class="col-xs-12 col-sm-5 col-sm-offset-3">
+                <ul id="socialHeader" class="list-inline">
+                  <li><i class="fa fa-facebook fa-2x yellowFont"></i></li>
+                  <li><i class="fa fa-twitter fa-2x yellowFont"></i></li>
+                  <li><i class="fa fa-instagram fa-2x yellowFont"></i></li>
+                  <li><i class="fa fa-pinterest fa-2x yellowFont"></i></li>
                   <li>
                     <div id="mc_embed">
                       <form action="//richlinegroup.us2.list-manage.com/subscribe/post?u=b6f74687c0d1b0a93dca80e4c&amp;id=a809606d72" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                         <div id="mc_embed_signup_scroll">
-                          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address"  data-toggle="tooltip" data-placement="bottom" title="**Insiders will automatically be entered to win" required>
+                          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
                           <div style="position: absolute; left: -5000px;"><input type="text" name="b_b6f74687c0d1b0a93dca80e4c_a809606d72" tabindex="-1" value=""></div>
                           <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button brownBackground yellowFont">
                         </div>
                       </form>
                     </div>
                   </li>
-                </ul>
+                </ul> 
+              </div>
             </div>
           </div>
         </div>
+
+        <div class="col-xs-12">
+          <div class="row">
+            <div class="col-xs-12 yellowBackground"></div>
+          </div>
+        </div>
+
+
+        <div class="container">          
+          <div class="row">
+            <nav class="navbar navbar-default navbar-static-top noMargin">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="migm-navbar-collapse">
+                    <span class="sr-only">Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="migm-navbar-collapse">
+                  <ul class="nav navbar-nav">
+                    <li><a href="./">HOME</a></li>
+                    <li><a href="#">RETAILERS</a></li>
+                    <li><a href="#">SWEEPSTAKES</a></li>
+                    <li><a href="#">EDUCATION</a></li>
+                    <li><a href="blog.php">INTERVIEWS</a></li>
+                    <li><a href="#">TRENDS</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
       </div>
+
     </div>
