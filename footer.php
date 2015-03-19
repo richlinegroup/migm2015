503b70e7222552446f4d65e82b5b3d4fade0e133
JonK_RLG <Jonathan.Kelly@richlinegroup.com>
2015-03-19 10:22:20 -0400

    updated the logos with the code from the index page

--- footer.php
+++ footer.php
@@ -1,45 +1,51 @@
-<footer>
-	<div class="container-fluid yellowBackground">
-		<div class="row">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-12">						
-						<ul class="list-inline text-center">
-							<li><img src="img/logos/amazonLogo.png"></li>
-							<li><img src="img/logos/bjLogo.png"></li>
-							<li><img src="img/logos/fredMeyerLogo.png"></li>
-							<li><img src="img/logos/helzbergLogo.png"></li>
-							<li><img src="img/logos/hsnLogo.png"></li>
-							<li><img src="img/logos/jcpLogo.png"></li>
-							<li><img src="img/logos/kayJewelerLogo.png"></li>
-							<li><img src="img/logos/kmartLogo.png"></li>
-							<li><img src="img/logos/kohlsLogo.png"></li>
-							<li><img src="img/logos/macysLogo.png"></li>
-							<li><img src="img/logos/qvcLogo.png"></li>
-							<li><img src="img/logos/samsLogo.png"></li>
-							<li><img src="img/logos/searsLogo.png"></li>
-							<li><img src="img/logos/walmartLogo.png"></li>
-						</ul>
-					</div>
+	
+            <div class="container yellowBackground">
+              <div class="row logos-top-row">
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-10">
+                  <a href="#"><img class="logo" src="img/logos/amazonLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/bjLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/fredMeyerLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/fingerhutLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/helzbergLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/hsnLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/jcpLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/kayJewelerLogo.png" /></a>
+                </div>
+                <div class="col-md-1">
+                </div>
+              </div>  
+              <div class="row logos-bottom-row">
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-11">
+                  <a href="#"><img class="logo" src="img/logos/kmartLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/kohlsLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/macysLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/qvcLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/samsLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/searsLogo.png" /></a>
+                  <a href="#"><img class="logo" src="img/logos/walmartLogo.png" /></a>
+                </div>
+
+              </div>
+            </div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 col-sm-offset-3 col-md-offset-3 text-center">
+					<small>Copyright &copy;2015 Richline Group. All rights reserved. This website, and the images, design, logos, trademarks
+						and trade names contained herein, other then those of our participating retailers, arethe exclusive property of Richline Group, Inc. 
+						Any unauthorized use will nr prosecuted to the fullest extent of the law.</small> <br><br>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-6 col-sm-offset-3 col-md-offset-3 text-center">
-				<small>Copyright &copy;2015 Richline Group. All rights reserved. This website, and the images, design, logos, trademarks
-					and trade names contained herein, other then those of our participating retailers, arethe exclusive property of Richline Group, Inc. 
-					Any unauthorized use will nr prosecuted to the fullest extent of the law.</small> <br><br>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 yellowBackground"><br></div>	
 			</div>
 		</div>
-	</div>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-xs-12 yellowBackground"><br></div>	
-		</div>
-	</div>
-</footer>
+	
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
