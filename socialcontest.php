6b1839b6667e67d6da20c267dde668bc1624f0e2
JonK_RLG <Jonathan.Kelly@richlinegroup.com>
2015-03-24 10:37:33 -0400

    pushing updated footer and social contest pages

--- socialcontest.php
+++ socialcontest.php
@@ -18,6 +18,11 @@
             </div>
           </div>
 
+          <div class="row">
+            <div class="col-xs-12">
+              
+            </div>
+          </div>
         </div>        
       </div>
     </div>
