60e275fa69ebfc3b8ac822500ed7fc7b6e3922b7
JonK_RLG <Jonathan.Kelly@richlinegroup.com>
2015-03-30 14:02:02 -0400

    removed logo carousel in footer for x-small mobile devices for performance reasons

--- footer.php
+++ footer.php
@@ -1,44 +1,46 @@
 	
 			<div class="container-fluid  dropShadowMain" style="max-width: 1200px;">
-				<div class="row logos-top-row yellowBackground ">
-					<div class="col-md-1">
-					</div>
-					<div class="col-md-10 img-responsive">
-					  <a href="http://www.amazon.com/b/ref=amb_link_357792882_10?ie=UTF8&amp;node=3246894011&amp;pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=center-8&amp;pf_rd_r=0T592AMJ47KBGDR88BSV&amp;pf_rd_t=101&amp;pf_rd_p=1325992762&amp;pf_rd_i=3367581"><img class="logo" src="img/logos/amazonLogo.png" /></a>
-					  <a href="http://www.bjs.com/gold-jewelry/gold-earrings.category.28009.26002.2003492.1"><img class="logo" src="img/logos/bjLogo.png" /></a>
-					  <a href="http://www.fredmeyerjewelers.com/mayisgoldmonth?CID=2L_MayIsGoldMonth"><img class="logo" src="img/logos/fredMeyerLogo.png" /></a>
-					  <a href="http://www.fingerhut.com/category/Jewelry-Watches/Gold-Jewelry/Mcatp/cat_8/9263.uts"><img class="logo" src="img/logos/fingerhutLogo.png" /></a>
-					  <a href="http://www.helzberg.com/category/collections/endura+gold.do?nType=1"><img class="logo" src="img/logos/helzbergLogo.png" /></a>
-					  <a href="http://www.hsn.com/shop/gold-jewelry-hoop/j00141?take=35&amp;facet=4231"><img class="logo" src="img/logos/hsnLogo.png" /></a>
-					  <a href="http://www.jcpenney.com/dotcom/jsp/search/results.jsp?fromSearch=true&amp;Ntt=14K+gold+hoops&amp;ruleZoneName=XGNSZone&amp;grView=&amp;_requestid=962465"><img class="logo" src="img/logos/jcpLogo.png" /></a>
-					  <a href="http://www.kay.com/SterlingSearch?storeId=10101&amp;catalogId=10001&amp;langId=-1&amp;globalSearchText=14K%20yelllow%20gold%20hoops&amp;searchText=14K%20yelllow%20gold%20hoops"><img class="logo" src="img/logos/kayJewelerLogo.png" /></a>
-					</div>
-					<div class="col-md-1">
-					</div>
-				</div>  
-				<div class="row logos-bottom-row yellowBackground">
-					<div class="col-md-1">
-					</div>
-					<div class="col-md-11 img-responsive">
-					  <a href="http://www.kmart.com/jewelry-gold-jewelry/b-20098"><img class="logo" src="img/logos/kmartLogo.png" /></a>
-					  <a href="http://www.kohls.com/catalog/gold-jewelry.jsp?CN=4294735791+4294719765"><img class="logo" src="img/logos/kohlsLogo.png" /></a>
-					  <a href="http://www1.macys.com/shop/jewelry-watches/gold?id=2904&edge=hybrid&cm_sp=us_hdr-_-jewelry-%26-watches-_-2904_gold"><img class="logo" src="img/logos/macysLogo.png" /></a>
-					  <a href="http://www.qvc.com/May+Is+Gold+Month.content.htmlmetaeSpot=1001%7C1001"><img class="logo" src="img/logos/qvcLogo.png" /></a>
-					  <a href="http://www.samsclub.com/sams/search/searchResults.jsp?searchCategoryId=all&amp;searchTerm=14K+yellow+gold+hoops&amp;fromHome=no&amp;_requestid=87561"><img class="logo" src="img/logos/samsLogo.png" /></a>
-					  <a href="http://www.sears.com/search=14k+yellow+gold+hoops?redirectType=CAT_REC_PRED&amp;viewItems=50&amp;autoRedirect=true&amp;storeId=10153&amp;levels=Jewelry"><img class="logo" src="img/logos/searsLogo.png" /></a>
-					  <a href="http://www.walmart.com/cp/1224930?povid=cat3891-env528539-moduleB050114-lLinkZone2_MayIsGoldMonth"><img class="logo" src="img/logos/walmartLogo.png" /></a>
+				<div class="hidden-xs">		
+					<div class="row logos-top-row yellowBackground ">
+						<div class="col-md-1">
+						</div>
+						<div class="col-md-10 img-responsive">
+						  <a href="http://www.amazon.com/b/ref=amb_link_357792882_10?ie=UTF8&amp;node=3246894011&amp;pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=center-8&amp;pf_rd_r=0T592AMJ47KBGDR88BSV&amp;pf_rd_t=101&amp;pf_rd_p=1325992762&amp;pf_rd_i=3367581"><img class="logo" src="img/logos/amazonLogo.png" /></a>
+						  <a href="http://www.bjs.com/gold-jewelry/gold-earrings.category.28009.26002.2003492.1"><img class="logo" src="img/logos/bjLogo.png" /></a>
+						  <a href="http://www.fredmeyerjewelers.com/mayisgoldmonth?CID=2L_MayIsGoldMonth"><img class="logo" src="img/logos/fredMeyerLogo.png" /></a>
+						  <a href="http://www.fingerhut.com/category/Jewelry-Watches/Gold-Jewelry/Mcatp/cat_8/9263.uts"><img class="logo" src="img/logos/fingerhutLogo.png" /></a>
+						  <a href="http://www.helzberg.com/category/collections/endura+gold.do?nType=1"><img class="logo" src="img/logos/helzbergLogo.png" /></a>
+						  <a href="http://www.hsn.com/shop/gold-jewelry-hoop/j00141?take=35&amp;facet=4231"><img class="logo" src="img/logos/hsnLogo.png" /></a>
+						  <a href="http://www.jcpenney.com/dotcom/jsp/search/results.jsp?fromSearch=true&amp;Ntt=14K+gold+hoops&amp;ruleZoneName=XGNSZone&amp;grView=&amp;_requestid=962465"><img class="logo" src="img/logos/jcpLogo.png" /></a>
+						  <a href="http://www.kay.com/SterlingSearch?storeId=10101&amp;catalogId=10001&amp;langId=-1&amp;globalSearchText=14K%20yelllow%20gold%20hoops&amp;searchText=14K%20yelllow%20gold%20hoops"><img class="logo" src="img/logos/kayJewelerLogo.png" /></a>
+						</div>
+						<div class="col-md-1">
+						</div>
+					</div>  
+					<div class="row logos-bottom-row yellowBackground">
+						<div class="col-md-1">
+						</div>
+						<div class="col-md-11 img-responsive">
+						  <a href="http://www.kmart.com/jewelry-gold-jewelry/b-20098"><img class="logo" src="img/logos/kmartLogo.png" /></a>
+						  <a href="http://www.kohls.com/catalog/gold-jewelry.jsp?CN=4294735791+4294719765"><img class="logo" src="img/logos/kohlsLogo.png" /></a>
+						  <a href="http://www1.macys.com/shop/jewelry-watches/gold?id=2904&edge=hybrid&cm_sp=us_hdr-_-jewelry-%26-watches-_-2904_gold"><img class="logo" src="img/logos/macysLogo.png" /></a>
+						  <a href="http://www.qvc.com/May+Is+Gold+Month.content.htmlmetaeSpot=1001%7C1001"><img class="logo" src="img/logos/qvcLogo.png" /></a>
+						  <a href="http://www.samsclub.com/sams/search/searchResults.jsp?searchCategoryId=all&amp;searchTerm=14K+yellow+gold+hoops&amp;fromHome=no&amp;_requestid=87561"><img class="logo" src="img/logos/samsLogo.png" /></a>
+						  <a href="http://www.sears.com/search=14k+yellow+gold+hoops?redirectType=CAT_REC_PRED&amp;viewItems=50&amp;autoRedirect=true&amp;storeId=10153&amp;levels=Jewelry"><img class="logo" src="img/logos/searsLogo.png" /></a>
+						  <a href="http://www.walmart.com/cp/1224930?povid=cat3891-env528539-moduleB050114-lLinkZone2_MayIsGoldMonth"><img class="logo" src="img/logos/walmartLogo.png" /></a>
+						</div>
 					</div>
 				</div>
 
 				<div class="row">
-					<div class="col-xs-10 col-sm-10 col-xs-offset-1 col-sm-offset-1 text-center topPadding">
+					<div class="col-xs-12 col-sm-10 col-sm-offset-1 text-center topPadding" style="background-color: white;">
 						<div class="">
 							<small>Copyright &copy;2015 Richline Group. All rights reserved. This website, and the images, design, logos, trademarks
 								and trade names contained herein, other then those of our participating retailers, arethe exclusive property of Richline Group, Inc. 
 								Any unauthorized use will be prosecuted to the fullest extent of the law.</small> <br><br>
 						</div>
 					</div>
-					<div class="row">
+					<div class="row hidden-xs">
 		                <div class="col-sm-12">
 		                    <hr style="margin-top: 0px;">
 		                </div>
