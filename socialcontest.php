b0103c27fd14795d359b9806bb62c3b53e907bb8
irinamanning <irina.manning@ny-10352.richlinegroup.local>
2015-04-01 16:25:49 -0400

    edited the contest pages

--- socialcontest.php
+++ socialcontest.php
@@ -25,84 +25,94 @@
       </div>
 
 
-      <div class="row" id="instagram-contest">
-        <div class="col-xs-12">
-          <img src="img/instagram-contest-rules.png" class="center-block"/>
-          <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Participate in Our Sweepstakes and Win</p>
-          <hr class="more-sweepstakes">
-          <div class="row center-block" style="margin-left: 60px;">
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/main-contest-button.png" class="hidden-xs contest-button" style="padding-top: 25px; padding-bottom: 40px;"></a>
-              <a href="#"><img src="img/main-contest-button.png" class="contest-button hidden-sm hidden-md hidden-lg padding-top-standard padding-bottom-standard"></a>
+      
+
+      
+      <cms:if k_page_name == "pinterest-contest">
+        <div class="row" id="pinterest-contest">
+          <div class="col-xs-12">
+            <div class="row">
+              <div class="col-md-8 col-xs-12">
+                <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Pinterest Contest Rules</p>
+                <hr class="thin margin-left" style="width: 550px; padding-bottom: 50px; margin-top: 0px;">
+                <img src="img/pinterest-contest-rules.png" class="margin-left"/>
+              </div>
+              <div class="col-md-4 col-xs-12">
+                <p class="about-subtitle yellowFont more-sweepstakes-title">Sign Up</p>
+                <hr class="thin margin-right" style="width: 310px; padding-bottom: 50px; margin-top: 0px;">
+                <img src="img/pinterest-form.png" class="img-responsive" style="max-width: 310px;"/>
+              </div>
             </div>
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/check-trend-button.png" class=" hidden-xs contest-button" style="margin-left: 45px; padding-top: 25px; padding-bottom: 40px;"></a>
-              <a href="#"><img src="img/pic-button.png" class="hidden-sm hidden-md hidden-lg contest-button padding-bottom-standard"></a>
+            <div class="row">
+              <div class="col-sm-12">
+                <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Participate in Our Sweepstakes and Win</p>
+                <hr class="more-sweepstakes">
+              </div>
             </div>
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/pin-button.png" class="contest-button padding-top-standard pull-right" style="margin-right: 60px; padding-top: 25px; padding-bottom: 40px;"></a>
+            <div class="row center-block" style="margin-left: 60px;">
+              <div class="col-sm-4" style="padding-top: 15px;">
+                <a href="#"><img src="img/main-contest-button.png" class="hidden-xs contest-button" style="padding-top: 25px; padding-bottom: 40px;"></a>
+                <a href="#"><img src="img/main-contest-button.png" class="contest-button hidden-sm hidden-md hidden-lg padding-top-standard padding-bottom-standard"></a>
+              </div>
+              <div class="col-sm-4" style="padding-top: 15px;">
+                <a href="#"><img src="img/check-trend-button.png" class=" hidden-xs contest-button" style="margin-left: 45px; padding-top: 25px; padding-bottom: 40px;"></a>
+                <a href="#"><img src="img/check-trend-button.png" class="hidden-sm hidden-md hidden-lg contest-button padding-bottom-standard"></a>
+              </div>
+              <div class="col-sm-4" style="padding-top: 15px;">
+                <a href="#"><img src="img/pic-button.png" class="contest-button padding-top-standard pull-right" style="margin-right: 60px; padding-top: 25px; padding-bottom: 40px;"></a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="row" id="pinterest-contest">
-        <div class="col-xs-12">
-          <div class="row">
-            <div class="col-md-8 col-xs-12">
-              <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Pinterest Contest Rules</p>
-              <hr class="thin margin-left" style="width: 550px; padding-bottom: 50px; margin-top: 0px;">
-              <img src="img/pinterest-contest-rules.png" class="margin-left"/>
-            </div>
-            <div class="col-md-4 col-xs-12">
-              <p class="about-subtitle yellowFont more-sweepstakes-title">Sign Up</p>
-              <hr class="thin margin-right" style="width: 310px; padding-bottom: 50px; margin-top: 0px;">
-              <img src="img/pinterest-form.png" class="img-responsive" style="max-width: 310px;"/>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12">
+      <cms:else />
+        <cms:if k_page_name == "main-contest">     
+          <div class="row" id="main-contest">
+            <div class="col-xs-12">
+              <img src="img/main-contest-rules.png" class="center-block"/>
+              <img src="img/sweepstakes-placeholder.png" class="center-block img-responsive" style="margin: auto; padding: 70px 75px 0px 75px"/>
               <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Participate in Our Sweepstakes and Win</p>
               <hr class="more-sweepstakes">
+              <div class="row center-block" style="margin-left: 60px;">
+                <div class="col-sm-4" style="padding-top: 15px;">
+                  <a href="#"><img src="img/pic-button.png" class="hidden-xs contest-button" style="padding-top: 25px; padding-bottom: 40px;"></a>
+                  <a href="#"><img src="img/pic-button.png" class="contest-button hidden-sm hidden-md hidden-lg padding-top-standard padding-bottom-standard"></a>
+                </div>
+                <div class="col-sm-4" style="padding-top: 15px;">
+                  <a href="#"><img src="img/check-trend-button.png" class=" hidden-xs contest-button" style="margin-left: 45px; padding-top: 25px; padding-bottom: 40px;"></a>
+                  <a href="#"><img src="img/check-trend-button.png" class="hidden-sm hidden-md hidden-lg contest-button padding-bottom-standard"></a>
+                </div>
+                <div class="col-sm-4" style="padding-top: 15px;">
+                  <a href="#"><img src="img/pin-button.png" class="contest-button padding-top-standard pull-right" style="margin-right: 60px; padding-top: 25px; padding-bottom: 40px;"></a>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="row center-block" style="margin-left: 60px;">
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/main-contest-button.png" class="hidden-xs contest-button" style="padding-top: 25px; padding-bottom: 40px;"></a>
-              <a href="#"><img src="img/main-contest-button.png" class="contest-button hidden-sm hidden-md hidden-lg padding-top-standard padding-bottom-standard"></a>
+        <cms:else />
+          <cms:if k_page_name == "instagram-contest">
+            <div class="row" id="instagram-contest">
+              <div class="col-xs-12">
+                <img src="img/instagram-contest-rules.png" class="center-block"/>
+                <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Participate in Our Sweepstakes and Win</p>
+                <hr class="more-sweepstakes">
+                <div class="row center-block" style="margin-left: 60px;">
+                  <div class="col-sm-4" style="padding-top: 15px;">
+                    <a href="#"><img src="img/main-contest-button.png" class="hidden-xs contest-button" style="padding-top: 25px; padding-bottom: 40px;"></a>
+                    <a href="#"><img src="img/main-contest-button.png" class="contest-button hidden-sm hidden-md hidden-lg padding-top-standard padding-bottom-standard"></a>
+                  </div>
+                  <div class="col-sm-4" style="padding-top: 15px;">
+                    <a href="#"><img src="img/check-trend-button.png" class=" hidden-xs contest-button" style="margin-left: 45px; padding-top: 25px; padding-bottom: 40px;"></a>
+                    <a href="#"><img src="img/pic-button.png" class="hidden-sm hidden-md hidden-lg contest-button padding-bottom-standard"></a>
+                  </div>
+                  <div class="col-sm-4" style="padding-top: 15px;">
+                    <a href="#"><img src="img/pin-button.png" class="contest-button padding-top-standard pull-right" style="margin-right: 60px; padding-top: 25px; padding-bottom: 40px;"></a>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/check-trend-button.png" class=" hidden-xs contest-button" style="margin-left: 45px; padding-top: 25px; padding-bottom: 40px;"></a>
-              <a href="#"><img src="img/check-trend-button.png" class="hidden-sm hidden-md hidden-lg contest-button padding-bottom-standard"></a>
-            </div>
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/pic-button.png" class="contest-button padding-top-standard pull-right" style="margin-right: 60px; padding-top: 25px; padding-bottom: 40px;"></a>
-            </div>
-          </div>
-        </div>
-      </div>
+          </cms:if>
+        </cms:if>
+      </cms:if>
 
-      <div class="row" id="main-contest">
-        <div class="col-xs-12">
-          <img src="img/main-contest-rules.png" class="center-block"/>
-          <img src="img/sweepstakes-placeholder.png" class="center-block img-responsive" style="margin: auto; padding: 70px 75px 0px 75px"/>
-          <p class="about-subtitle yellowFont margin-left more-sweepstakes-title">Participate in Our Sweepstakes and Win</p>
-          <hr class="more-sweepstakes">
-          <div class="row center-block" style="margin-left: 60px;">
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/pic-button.png" class="hidden-xs contest-button" style="padding-top: 25px; padding-bottom: 40px;"></a>
-              <a href="#"><img src="img/pic-button.png" class="contest-button hidden-sm hidden-md hidden-lg padding-top-standard padding-bottom-standard"></a>
-            </div>
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/check-trend-button.png" class=" hidden-xs contest-button" style="margin-left: 45px; padding-top: 25px; padding-bottom: 40px;"></a>
-              <a href="#"><img src="img/check-trend-button.png" class="hidden-sm hidden-md hidden-lg contest-button padding-bottom-standard"></a>
-            </div>
-            <div class="col-sm-4" style="padding-top: 15px;">
-              <a href="#"><img src="img/pin-button.png" class="contest-button padding-top-standard pull-right" style="margin-right: 60px; padding-top: 25px; padding-bottom: 40px;"></a>
-            </div>
-          </div>
-        </div>
-      </div>
 
 
     <!--   </div>
