060a812d5c7b872de0af9cb0ed2c9cff959034c2
irinamanning <irina.manning@NY-10352.local>
2015-04-10 15:23:58 -0400

    changed the text on blog.php to brown

--- blog.php
+++ blog.php
@@ -152,7 +152,7 @@
 
                   <div class="col-xs-12 col-sm-12 col-md-8 leftBorderYellow" style="padding-left: 40px; padding-right: 75px;">
                     <div class="hidden-xs hidden-sm ">
-                      <h1 class="yellowFont new-expert-name noMargin noPadding"><cms:show k_page_title /></h1>
+                      <h1 class="brownFont new-expert-name noMargin noPadding"><cms:show k_page_title /></h1>
                       <h5 class="brownFont blog-page-prof-title noPadding" style="display: inline-block; font-size: 13.5"><i><strong><cms:show professional_title /></strong></i></h5>
                       <ul class="list-inline" style="padding-top: 10px;">
                         <li ><i class="fa fa-facebook fa-2x yellowFont"></i></li>
