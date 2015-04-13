32850b5a74eaa5658276d459921acd705847aa24
JonK_RLG <Jonathan.Kelly@richlinegroup.com>
2015-04-13 14:44:23 -0400

    updated facebook tracking code

--- header.php
+++ header.php
@@ -29,21 +29,22 @@
     <![endif]-->
 
     <!-- Facebook Conversion Code for Leads - MIGM Site -->
-    <script>(function() {
-      var _fbq = window._fbq || (window._fbq = []);
-      if (!_fbq.loaded) {
-        var fbds = document.createElement('script');
-        fbds.async = true;
-        fbds.src = '//connect.facebook.net/en_US/fbds.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(fbds, s);
-        _fbq.loaded = true;
-      }
+    <script>(function () {
+        var _fbq = window._fbq || (window._fbq = []);
+        if (!_fbq.loaded) {
+            var fbds = document.createElement('script');
+            fbds.async = true;
+            fbds.src = '//connect.facebook.net/en_US/fbds.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(fbds, s);
+            _fbq.loaded = true;
+        }
+    _fbq.push(['addPixelId', '6024541387812']);
     })();
     window._fbq = window._fbq || [];
-    window._fbq.push(['track', 'PixelInitialized', {}]);
+    window._fbq.push(['track', '6024541387812', {'value':'0.00','currency':'USD'}]);
     </script>
-    <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6024541387812&amp;ev=PixelInitialized " /></noscript>
+    <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6024541387812&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
 
 
     <script>
